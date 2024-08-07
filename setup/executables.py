d282dad0517ee8651811f4defe2a204b489b10b3
bitesizing <r.raimundo2001@gmail.com>
2024-08-07 19:49:16 +0100

    . Cleaned up executable

--- setup/executables.py
+++ setup/executables.py
@@ -23,34 +23,6 @@ def toggle_abstract_collapsing(become_open: bool, limit: int = -1, copy_files: b
         
         file.write_file(copy=copy_files)
 
-def update_zotero_links(limit: int = -1, copy_files: bool = False):
-    """ Update zotero links to the new format. """
-
-    for file in yield_papers(limit=limit):
-        file: ObsidianFile
-        
-        # Find citation key and zotero link
-        citation_key = file.return_property_values('citation key')[0]
-        
-        # Find old format zotero link
-        for link in file.return_property_values('links'):
-            if 'zotero://select/' in link:
-                zotero_link = link
-                break
-        
-        # Replace and update properties
-        new_zotero_link = f'zotero://select/items/@{citation_key}'
-        file.replace_property_value(zotero_link, new_zotero_link)
-        file.write_file(copy=copy_files)  # write files
-
-def change_tags_to_lowercase(limit: int = -1, copy_files: bool = False):
-    """ Change the word 'Tags' in the properties to 'tags', meaning that tags come up in search results. """
-
-    for file in yield_papers(limit=limit):
-        file: ObsidianFile
-        file.make_properties_lowercase()
-        file.write_file(copy=copy_files)  # write files
-
 def split_links_property(limit: int = -1, copy_files: bool = False):
     """ Split old format 'links' property into 'DOI' and 'Zotero' properties. """
 
@@ -79,27 +51,6 @@ def split_links_property(limit: int = -1, copy_files: bool = False):
         paper.properties['zotero'] = zotero if zotero else ""
         paper.update_flat_properties_from_properties_dict()
         paper.write_file(copy=copy_files)
-
-def update_DOIs_from_root_level_papers(limit: int = -1):
-    """ Update broken DOI links using imported papers from the root level directory. """
-
-    for root_level_paper in yield_papers(limit=limit, exclude_subfolders=True):
-        root_level_paper: ObsidianFile
-        
-        # Get new DOI
-        doi = root_level_paper.return_property_values('links')[0]
-        
-        # Find corresponding paper in the vault by matching citation key
-        citation_key = root_level_paper.return_property_values('citation key')[0]
-        
-        for matching_paper in yield_papers():
-            matching_paper: ObsidianFile
-            old_link = matching_paper.return_property_values('links')[0]
-
-            if matching_paper.return_property_values('citation key')[0] == citation_key and old_link != doi:
-                matching_paper.replace_property_value(old_link, doi)
-                matching_paper.write_file()
-                break  # matching paper found, no need to keep iterating
 
 def add_missing_dois(limit: int = -1, copy_files: bool = False):
     """ Add missing DOIs to papers in the vault. """
