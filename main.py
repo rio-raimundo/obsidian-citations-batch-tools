9c289caa1daa1f72fedaed497cb4767f9f6d77dd
bitesizing <r.raimundo2001@gmail.com>
2024-12-16 11:56:23 +0100

    .! Updated docstring in main to provide information about new rename_files function

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 #! %autoreload 3
 """ Main file from which to run the project."""
 
-from helpers import ObsidianNote, process_articles
+from helpers import ObsidianNote, process_articles, rename_articles
 import constants as c
 
-""" EXAMPLE USAGE OF THE CODE. The general process is as follows: 
-    - First, call the decorator `process_articles` with the desired arguments. This will process the articles in the vault and make them available to the function you define.
-    - Define a function that takes an `ObsidianNote` object as an argument. This function will be executed on each article in the vault.
+"""
+This project currently contains two parent 'decorators' (the functions with the @ symbol) that can be used to modify ObsidianNote objects representing articles in a vault. These decorators are defined in the helpers/decorators.py file, and are imported here.
+
+The two decorators are:
+    - process_articles: This is the main decorator of this project. It takes in a user-defined function and runs it for each note in the vault.
+    - rename_articles: This is a specialised decorator designed to enable the mass renaming of files. Its architecture needs to be separate from process_articles because it also goes through and updates all links to the renamed files. It also takes in a user-defined function, which must return the desired new name of the file.
+
+Information on how to use both decorators is provided in the docstrings of the helpers/decorators.py file. Additionally, an extended example of how to use the process_articles decorator (the main decorator you will be using) is provided below. This example also includes an example of integrating with a BibTex file to pull extra information into your Obsidian notes.
 """
 @process_articles(limit=-1, exclude_subfolders=False, write=True)
 def update_journals(obsidian_note: ObsidianNote):
