32c13cfbff420ea77e44a20974c030f8616519a2
bitesizing <r.raimundo2001@gmail.com>
2024-12-16 11:22:07 +0100

    . Changed main to use the renamer decorator with if statement

--- main.py
+++ main.py
@@ -4,18 +4,17 @@
 """ Main file from which to run the project."""
 
 from os.path import join
-from helpers import ObsidianNote, process_articles, NoteRenamer
+from helpers import ObsidianNote, process_articles, rename_articles
 import constants as c
 
-# Initialise file renamer
-renamer = NoteRenamer()
+# Use the note renamer decorator
 
-@process_articles(limit=1, exclude_subfolders=False, write=False)
-def files_to_rename(obsidian_note: ObsidianNote):
-    old_name = obsidian_note.filename.rsplit(".md", 1)[0]
-    new_name = old_name
-    renamer.add(obsidian_note.filepath, old_name, new_name)
+@rename_articles(limit=2, exclude_subfolders=False)
+def rename_files(obsidian_note: ObsidianNote):
+    if 'Mahowald' in obsidian_note.filename:
+        old_name = obsidian_note.filename.rsplit(".md", 1)[0]
+        new_name = old_name + '2'
+        return old_name, new_name
 
-# Finally, call your function to execute it for the specified number of articles in a vault
-files_to_rename()
-renamer.rename_files()
+# Call the function
+rename_files()
