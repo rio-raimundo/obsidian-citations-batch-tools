6b6e09cf9a99b6aea167407431758144aecc238d
bitesizing <r.raimundo2001@gmail.com>
2024-12-16 10:51:46 +0100

    * Note renamer now renames all types of links, including those to headings, those to blocks, absolute links, piped links, and those in tables!

--- helpers/note_renamer.py
+++ helpers/note_renamer.py
@@ -1,5 +1,5 @@
 """ File allowing the batch renaming of notes. This is difficult to do normally because links are not updated if files are renamed outside of Obsidian. """
-import re
+import regex
 
 from dataclasses import dataclass
 from . import yield_notes, ObsidianNote
@@ -36,14 +36,37 @@ class NoteRenamer:
         for obsidian_note in yield_notes(-1, exclude_subfolders=False):
             obsidian_note: ObsidianNote
 
-            # Find all links in page
-            pattern = re.compile(r"\[\[.*?\]\]")
+            """ First we can define the regex patterns which will be used:
+                - Full link pattern is a simple pattern that matches any text in the form [[...]], including the brackets
+
+                - Link name pattern is a complex pattern attempting to match the 'true link' portion of linked text (referred to as 'l'):
+                    - This includes cases of links to headings ('l#'), links to blocks ('l#^'), links with renames ('l|'), links in tables ('l\|'), and absolute links ('/l').
+                    - The link has three main parts. First is a lookbehind matching either '[[' or '/' (the start of a link).
+                    - Second is the actual link name, which is any text that is not '#', '|', '/' or '\' (the end of a link).
+                    - Finally, there is a lookahead matching either ']]', '|', '#', or '\' (the end of a link).
+             """
+            full_link_pattern = regex.compile(r"\[\[.*?\]\]")
+            link_name_pattern = regex.compile(r"(?<=\[\[|\/)[^#|\/\\]*(?=\]\]|\||#|\\)")
+
+            # Iterate through each line of the note and check links
             for idx in range(len(obsidian_note.body_text)):
-                matches = re.findall(pattern, obsidian_note.body_text[idx])
+                full_links = regex.findall(full_link_pattern, obsidian_note.body_text[idx])
                 
-                for match in matches:
-                    match: str = match[2:-2]
-                    obsidian_note.body_text[idx] = obsidian_note.body_text[idx].replace(match, self.replace_link(match))
+                # For each link, including the brackets
+                for full_link in full_links:
+                    # we have to pull out the link name, accounting for all possible phrasings (absolute links, links to headings, links with renames, etc.)
+                    # This can be done using the link_name_pattern - can put this into regex101.com to see how it works
+                    link_name = regex.search(link_name_pattern, full_link).group(0)
+
+                    # Get the 'new' link name (replaces old name with new name if it exists, otherwise just returns the old name)
+                    new_link_name = self.replace_link(link_name)
+                    print(f"{obsidian_note.filename}: {link_name} -> {new_link_name}")
+
+                    # Sub the new link name into the full link only at the occurrences given by the link_name_pattern
+                    new_full_link = regex.sub(link_name_pattern, new_link_name, full_link)
+                    
+                    # Replace the full link in the text with the new full link
+                    obsidian_note.body_text[idx] = obsidian_note.body_text[idx].replace(full_link, new_full_link)
             
             # Write the file
             obsidian_note.write_file()
