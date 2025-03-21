8ff2f04ea23b7d8ebc0a5b347a35dff45086b7f6
bitesizing <r.raimundo2001@gmail.com>
2025-03-21 09:26:41 +0100

    PERSONAL: changed main

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 #! %autoreload 3
 """ Main file from which to run the project."""
 
+from os import rename
 from pybtex.database import Person, Entry
 from helpers import ObsidianNote, process_articles, rename_articles
 import constants as c
+import re
 
 """
 This project currently contains two parent 'decorators' (the functions with the @ symbol) that can be used to modify ObsidianNote objects representing articles in a vault. These decorators are defined in the helpers/decorators.py file, and are imported here.
@@ -16,29 +18,17 @@ The two decorators are:
 
 Information on how to use both decorators is provided in the docstrings of the helpers/decorators.py file. Additionally, an extended example of how to use the process_articles decorator (the main decorator you will be using) is provided below. This example also includes an example of integrating with a BibTex file to pull extra information into your Obsidian notes.
 """
-@process_articles(limit=-1, write=False)
-def update_journals(obsidian_note: ObsidianNote):
-    """ EXAMPLE FUNCTION.
-        - Updates the 'journal' property of an ObsidianNote object, or adds it if it does not have it.
-        - Also moves it to a specificied location in the properties list (specified in initial variable).
-    """
-    location = 2
+def starts_with_dashed_date(s):
+    return bool(re.match(r'^\d{2}-\d{2}-\d{2}', s))
 
-    # Delete current reference to property if it already exists
-    # NOTE: we could check if this is in the right place, but read/write operations are so fast this is fine
-    if 'journal' in obsidian_note.properties:
-        del obsidian_note.properties['journal']
-    
-    # Add in the journal property into the specified location
-    # NOTE: this call relies on obsidian_note.bibtex_data(), which reads the BibTex data using the `c.bibtex_path` constant, and assumes that the ObsidianNote has a property 'citation key' that contains the BibTex citation key. The name of this property can be specified in the constants file.
+@rename_articles(limit=-1, yield_all_files=True)
+def rename_dates(obsidian_note: ObsidianNote):
+    starts_with_date = re.match(r'^\d{2}-\d{2}-\d{2}', obsidian_note.filename)
 
-    # Returns the value of the 'journal' key in the BibTex data, or None if it does not exist
-    bibdata: Entry = obsidian_note.bibtex_data
-    if not bibdata: return  # skip articles with non bibdata!
-    journal = bibdata.fields.get('journal', 'unspecified')  # second argumnet is the default value
-
-    # Insert the property at a specified location
-    obsidian_note.insert_property_at_location('journal', journal, location)
+    if starts_with_date:
+        # Replace with dots
+        new_filename = re.sub(r'^(\d{2})-(\d{2})-(\d{2})', r'\1.\2.\3', obsidian_note.filename)
+        return new_filename
 
 # Finally, call your function to execute it for the specified number of articles in a vault
-update_journals()
+rename_dates()
