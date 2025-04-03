8d4668ceafdea2b7b9f131a2e382963fcdddf218
bitesizing <r.raimundo2001@gmail.com>
2025-04-03 11:14:51 +0200

    [INTERNAL] cahnges to main

--- main.py
+++ main.py
@@ -18,17 +18,13 @@ The two decorators are:
 
 Information on how to use both decorators is provided in the docstrings of the helpers/decorators.py file. Additionally, an extended example of how to use the process_articles decorator (the main decorator you will be using) is provided below. This example also includes an example of integrating with a BibTex file to pull extra information into your Obsidian notes.
 """
-def starts_with_dashed_date(s):
-    return bool(re.match(r'^\d{2}-\d{2}-\d{2}', s))
+@process_articles(limit=1)
+def add_year(obsidian_note: ObsidianNote):
+    btex_data = obsidian_note.bibtex_data
+    if (not hasattr(btex_data, 'fields') or 'date' not in btex_data.fields): return
 
-@rename_articles(limit=-1, yield_all_files=True)
-def rename_dates(obsidian_note: ObsidianNote):
-    starts_with_date = re.match(r'^\d{2}-\d{2}-\d{2}', obsidian_note.filename)
-
-    if starts_with_date:
-        # Replace with dots
-        new_filename = re.sub(r'^(\d{2})-(\d{2})-(\d{2})', r'\1.\2.\3', obsidian_note.filename)
-        return new_filename
+    year = obsidian_note.bibtex_data.fields['date'].split('-')[0]
+    obsidian_note.insert_property_near_another('year', year, 'authors', insert_after=False, override_existing=True)
 
 # Finally, call your function to execute it for the specified number of articles in a vault
-rename_dates()
+add_year()
